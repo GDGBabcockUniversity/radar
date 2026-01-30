abdffbdb314b83d4d91bd4aeddcb0898f2e20eb5
Victor Ibironke <victoribironke2019@gmail.com>
2026-01-30 10:53:21 +0100

    fixed lint errors

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getRandomElement(arr: any[]) {
+export function getRandomElement(arr: string[]) {
   const randomIndex = Math.floor(Math.random() * arr.length);
 
   return arr[randomIndex];
