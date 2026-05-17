67fd90ac604f0b803869feaa048fffcfb7c9ddd1
Victor Ibironke <victoribironke2019@gmail.com>
2026-05-17 10:08:54 +0100

    feat: include seriesGroup and series schemas in exported schemaTypes

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -3,5 +3,7 @@ import category from "./category";
 import post from "./post";
 import author from "./author";
 import teamMember from "./teamMember";
+import seriesGroup from "./seriesGroup";
+import series from "./series";
 
-export const schemaTypes = [post, author, category, blockContent, teamMember];
+export const schemaTypes = [post, author, category, blockContent, teamMember, seriesGroup, series];
