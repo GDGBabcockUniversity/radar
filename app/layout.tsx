ed98d4f9d315ad6107ce944a6901897b76b3f873
Victor Ibironke <victoribironke2019@gmail.com>
2026-06-11 14:54:00 +0100

    feat: add support for multiple fonts and theme provider in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Google_Sans_Flex } from "next/font/google";
+import { Google_Sans_Flex, Caveat, Merriweather } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/next";
 import { cn } from "./lib/utils";
+import ThemeProvider from "./components/ThemeProvider";
 
 const googleSansFlex = Google_Sans_Flex({
   variable: "--font-google-sans-flex",
@@ -28,17 +29,17 @@ const googleSansFlex = Google_Sans_Flex({
 //   weight: ["400", "500", "600", "700"],
 // });
 
-// const merriweather = Merriweather({
-//   variable: "--font-merriweather",
-//   subsets: ["latin"],
-//   weight: ["400", "700"],
-// });
+const merriweather = Merriweather({
+  variable: "--font-merriweather",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
 
-// const caveat = Caveat({
-//   variable: "--font-caveat",
-//   subsets: ["latin"],
-//   weight: ["400", "700"],
-// });
+const caveat = Caveat({
+  variable: "--font-caveat",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata: Metadata = {
   title: "RADAR | GDG Babcock",
@@ -51,24 +52,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn(googleSansFlex.variable, "antialiased")}>
-        <Toaster
-          theme="dark"
-          position="top-right"
-          toastOptions={{
-            style: {
-              background: "#000",
-              color: "#fff",
-              border: "1px solid rgba(255,255,255,0.1)",
-              borderRadius: "6px",
-              fontSize: "14px",
-              fontWeight: 500,
-            },
-          }}
-        />
-        {children}
-        <Analytics />
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body
+        className={cn(
+          googleSansFlex.variable,
+          merriweather.variable,
+          caveat.variable,
+          "antialiased",
+        )}
+      >
+        <ThemeProvider>
+          <Toaster
+            theme="dark"
+            position="top-right"
+            toastOptions={{
+              style: {
+                background: "#000",
+                color: "#fff",
+                border: "1px solid rgba(255,255,255,0.1)",
+                borderRadius: "6px",
+                fontSize: "14px",
+                fontWeight: 500,
+              },
+            }}
+          />
+          {children}
+          <Analytics />
+        </ThemeProvider>
       </body>
     </html>
   );
