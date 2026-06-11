a447771d90078b181b1b428c14d50a1816fe228a
Victor Ibironke <victoribironke2019@gmail.com>
2026-06-11 18:20:18 +0100

    feat: update sections index with new LatestIssueSection and SpotlightSection exports

--- app/sections/index.ts
+++ app/sections/index.ts
@@ -1,4 +1,5 @@
 export { default as HeroSection } from "./HeroSection";
-export { default as FeaturedSection } from "./FeaturedSection";
+export { default as LatestIssueSection } from "./LatestIssueSection";
+export { default as SpotlightSection } from "./SpotlightSection";
 export { default as PastEditionsSection } from "./PastEditionsSection";
 export { default as NewsletterSection } from "./NewsletterSection";
